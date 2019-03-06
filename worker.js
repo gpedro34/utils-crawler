2dc6353d89a42bca0e53a98814ad60ab4b5b89ee
Gonçalo Pedro <gpedro34@gmail.com>
2019-03-06 02:47:50 +0000

    chalk removal and RECHECK_INTERVAL is now in hours instead of minutes

--- worker.js
+++ worker.js
@@ -1,19 +1,18 @@
 'use strict';
 
 const exit = require('exit');
-const chalk = require('chalk');
 
 const utils = require('./lib/utils');
 const logger = require('./lib/logger/logger');
 // Defaults
 const defaults = require('./config/defaults');
-const RECHECK_INTERVAL = process.env.RECHECK_INTERVAL || defaults.app.recheckInterval;
+const RECHECK_INTERVAL = process.env.RECHECK_INTERVAL*60 || defaults.app.recheckInterval*60;
 // DB connection
 const checks = require('./lib/db/connection').checks;
 
 // Fire work
 (async()=>{
-	logger.info(`${process.pid} => Sucessfully booted!`);
+	// logger.info(`${process.pid} => Sucessfully booted!`);
 	await checks.listTodo(RECHECK_INTERVAL);
 })();
 
