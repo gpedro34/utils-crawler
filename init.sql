0e486bdfadb888422a3a82b8972a37cf5f25fd14
Gonçalo Pedro <gpedro34@gmail.com>
2019-03-06 02:48:17 +0000

    updated database tables structure

--- init.sql
+++ init.sql
@@ -5,14 +5,13 @@ use brs_crawler;
 
 -- ssl_checks table
 CREATE TABLE IF NOT EXISTS ssl_checks (
-	ssl_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-	peer_id INT UNSIGNED NOT NULL,
-	ssl_status INT UNSIGNED NULL,
-	ssl_from TIMESTAMP NULL,
-	ssl_to TIMESTAMP NULL,
-	last_scanned TIMESTAMP DEFAULT CURRENT_TIMESTAMP() NOT NULL,
+		ssl_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+		ssl_status INT UNSIGNED NULL,
+		ssl_from TIMESTAMP NULL,
+		ssl_to TIMESTAMP NULL,
+		hash VARCHAR(32) NOT NULL,
   CONSTRAINT ssl_checks_pk PRIMARY KEY (ssl_id),
-  CONSTRAINT ssl_checks_peer_id_uk UNIQUE KEY (peer_id)
+  CONSTRAINT ssl_checks_hash_uk UNIQUE KEY (hash)
 )
 AUTO_INCREMENT=1
 ENGINE=InnoDB
@@ -22,10 +21,10 @@ COLLATE=utf8mb4_general_ci;
 
 -- loc_checks table
 CREATE TABLE IF NOT EXISTS loc_checks (
-	loc_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-	country_city VARCHAR(191) NOT NULL,
+		loc_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+		country_city VARCHAR(191) NOT NULL,
   CONSTRAINT loc_checks_pk PRIMARY KEY (loc_id),
-  CONSTRAINT loc_checks_city_uk UNIQUE KEY (country_city)
+  CONSTRAINT loc_checks_country_city_uk UNIQUE KEY (country_city)
 )
 AUTO_INCREMENT=1
 ENGINE=InnoDB
@@ -36,6 +35,7 @@ COLLATE=utf8mb4_general_ci;
 -- checks table
 CREATE TABLE IF NOT EXISTS checks (
 		id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+		hash VARCHAR(32) NOT NULL,
 		peer_id INT UNSIGNED NOT NULL,
 		blocked SMALLINT UNSIGNED DEFAULT 0 NOT NULL,
 		ip VARCHAR(45) DEFAULT NULL,
@@ -44,9 +44,8 @@ CREATE TABLE IF NOT EXISTS checks (
 		api INT UNSIGNED DEFAULT NULL,
 		last_scanned TIMESTAMP DEFAULT CURRENT_TIMESTAMP() NOT NULL,
 	CONSTRAINT checks_pk PRIMARY KEY (id),
-	CONSTRAINT checks_peer_id_uk UNIQUE KEY (peer_id)
+	CONSTRAINT checks_hash_uk UNIQUE KEY (hash)
 )
-AUTO_INCREMENT=1
 ENGINE=InnoDB
 DEFAULT CHARSET=utf8mb4
 COLLATE=utf8mb4_general_ci;
